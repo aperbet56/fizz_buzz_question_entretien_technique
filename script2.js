cdbe99d505c0013c4cd8cfee03ced5708db33f90
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-12-12 17:52:52 +0100

    Création du fichier script2.js

---
+++ script2.js
@@ -0,0 +1,13 @@
+/**
+ * Énoncé du problème :
+
+Afficher les nombres de 1 à n (ou les stocker dans un tableau).
+
+- Si le nombre est divisible par 3, afficher « Fizz ».
+
+- Si le nombre est divisible par 5, afficher « Buzz ».
+
+- Si le nombre est divisible par 3 et 5, afficher « FizzBuzz ».
+
+- Si aucune des conditions précédentes n'est remplie, afficher le nombre.
+ */
